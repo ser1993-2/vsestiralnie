c03dc1b89e4dbe538d1c20f700f7033b80f8c93c
ser1993-2 <ser-video3@yandex.ru>
2020-05-25 01:49:08 +0400

    app

--- config/app.php
+++ config/app.php
@@ -80,7 +80,7 @@ return [
     |
     */
 
-    'locale' => 'en',
+    'locale' => 'ru',
 
     /*
     |--------------------------------------------------------------------------
@@ -165,7 +165,7 @@ return [
         /*
          * Package Service Providers...
          */
-
+        Alexusmai\YandexMetrika\YandexMetrikaServiceProvider::class,
         /*
          * Application Service Providers...
          */
@@ -226,6 +226,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'YandexMetrika' => Alexusmai\YandexMetrika\YandexMetrikaFacade::class,
 
     ],
 
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use \Carbon\Carbon;
+use \App\User;
+use \App\Models\Stats;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +14,30 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UserSeeder::class);
+        User::insert([
+            'name' => 'Admin',
+            'email' => 'admin@admin.ru',
+            'password' => Hash::make('123456'),
+            'isAdmin' => 1,
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]);
+
+        $datas = [
+            ['name' => 'Яндекс', 'alias' => 'Yandex'],
+            ['name' => 'Яндекс.Директ', 'alias' => 'Yandex: Direct'],
+            ['name' => 'Новые посетитиели', 'alias' => 'yandex_search'],
+            ['name' => 'Остальной трафик', 'alias' => 'other'],
+            ['name' => 'Nadavi', 'alias' => 'nadavi'],
+        ];
+
+        foreach ($datas as $data) {
+            Stats::insert([
+                'name' => $data['name'],
+                'alias' => $data['alias'],
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ]);
+        }
     }
 }
